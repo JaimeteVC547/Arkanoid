d8e8279183c4c236f2e9c699e4a44e0bbc260878
Salvador073 <danielsalvadormartinez07@gmail.com>
2026-03-26 14:20:57 +0100

    Pala hecha

--- main.js
+++ main.js
@@ -1,15 +1,46 @@
-const config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  backgroundColor: '#000',
-  scene: {
-    create: create
-  }
-};
+let moverIzquierda = false;
+let moverDerecha = false;
 
-new Phaser.Game(config);
+let palaX = 200; // posición inicial
+let velocidad = 5;
 
-function create() {
-  this.add.text(300, 250, 'Arkanoid');
+document.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowLeft" || e.key === "a"|| e.key === "A") {
+        moverIzquierda = true;
+    }
+    if (e.key === "ArrowRight" || e.key === "d"||e.key === "D") {
+        moverDerecha = true;
+    }
+});
+
+document.addEventListener("keyup", (e) => {
+    if (e.key === "ArrowLeft" || e.key === "a"|| e.key === "A") {
+        moverIzquierda = false;
+    }
+    if (e.key === "ArrowRight" || e.key === "d" ||e.key === "D") {
+        moverDerecha = false;
+    }
+});
+
+
+
+function actualizar() {
+    // mover pala
+    if (moverIzquierda) {
+        palaX -= velocidad;
+    }
+    if (moverDerecha) {
+        palaX += velocidad;
+    }
+
+    // evitar que se salga de la pantalla
+    if (palaX < 0) palaX = 0;
+    if (palaX > 400) palaX = 400; // ajusta según tu ancho
+
+    // aplicar posición en CSS
+    document.querySelector(".pala").style.left = palaX + "px";
+
+    requestAnimationFrame(actualizar);
 }
+
+actualizar();
